5e2ee288dbad2c5329ff1e5b13cfba85dfb9136c
Antti Haavikko <antti.haavikko@focus.fi>
2020-10-04 19:58:05 +0300

    Add loader and blinders

---
+++ Assets/SceneChanger/Blinders.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinders : MonoBehaviour
+{
+    public Transform left, right;
+    public bool startsOpen, openAtStart = true;
+
+    private float duration = 0.3f;
+    private bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = startsOpen;
+
+        if (startsOpen) return;
+
+        left.transform.localScale = new Vector3(1f, 1f, 1f);
+        right.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        if(openAtStart)
+            Invoke("Open", 0.5f);
+    }
+
+    public void Close()
+    {
+        if (!isOpen) return;
+
+        Tweener.Instance.ScaleTo(left, Vector3.one, duration, 0f, TweenEasings.BounceEaseOut);
+        Tweener.Instance.ScaleTo(right, Vector3.one, duration, 0f, TweenEasings.BounceEaseOut);
+
+        if(AudioManager.Instance)
+        {
+            //AudioManager.Instance.PlayEffectAt(12, Vector3.zero, 1.107f);
+            //AudioManager.Instance.PlayEffectAt(14, Vector3.zero, 1.177f);
+            //AudioManager.Instance.PlayEffectAt(19, Vector3.zero, 1.348f);
+        }
+
+        Invoke("Clang", duration * 0.9f);
+
+        isOpen = false;
+    }
+
+    public void Open()
+    {
+        Tweener.Instance.ScaleTo(left, new Vector3(0f, 1f, 1f), duration, 0f, TweenEasings.BounceEaseOut);
+        Tweener.Instance.ScaleTo(right, new Vector3(0f, 1f, 1f), duration, 0f, TweenEasings.BounceEaseOut);
+
+        if(AudioManager.Instance)
+        {
+            //AudioManager.Instance.PlayEffectAt(12, Vector3.zero, 1.107f);
+            //AudioManager.Instance.PlayEffectAt(14, Vector3.zero, 1.177f);
+            //AudioManager.Instance.PlayEffectAt(19, Vector3.zero, 1.348f);
+        }
+
+        isOpen = true;
+    }
+
+    public float GetDuration()
+    {
+        return duration;
+    }
+
+    void Clang()
+    {
+        if (AudioManager.Instance)
+        {
+            //AudioManager.Instance.PlayEffectAt(11, Vector3.zero, 1.193f);
+            //AudioManager.Instance.PlayEffectAt(19, Vector3.zero, 1.159f);
+            //AudioManager.Instance.PlayEffectAt(21, Vector3.zero, 1.322f);
+            //AudioManager.Instance.PlayEffectAt(22, Vector3.zero, 1.284f);
+            //AudioManager.Instance.PlayEffectAt(15, Vector3.zero, 1.511f);
+            //AudioManager.Instance.PlayEffectAt(3, Vector3.zero, 1.605f);
+        }
+
+    }
+}
---
+++ Assets/SceneChanger/SceneChanger.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Blinders blinders;
+    public Transform spinner;
+    public string sceneToLoadAtStart;
+    //public GameCursor cursor;
+    public Canvas canvas;
+    public GameObject startCam;
+
+    private string sceneToLoad;
+    private AsyncOperation operation;
+
+    private static SceneChanger instance = null;
+    public static SceneChanger Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(instance.gameObject);
+    }
+
+    private void Start()
+    {
+        if (!string.IsNullOrEmpty(sceneToLoadAtStart))
+            ChangeScene(sceneToLoadAtStart, true);
+    }
+
+    public void AttachCamera()
+    {
+        canvas.worldCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(operation != null && operation.isDone)
+        {
+            operation = null;
+            Invoke("After", 0.1f);
+        }
+    }
+
+    void After()
+    {
+        blinders.Open();
+        Tweener.Instance.ScaleTo(spinner, Vector3.zero, 0.2f, 0f, TweenEasings.QuadraticEaseIn);
+    }
+
+    public void ChangeScene(string sceneName, bool silent = false)
+    {
+         if(!silent)
+        {
+            //AudioManager.Instance.DoButtonSound();
+        }
+
+        if (startCam)
+            startCam.SetActive(true);
+
+        blinders.Close();
+        Tweener.Instance.ScaleTo(spinner, Vector3.one, 0.2f, 0f, TweenEasings.BounceEaseOut);
+        sceneToLoad = sceneName;
+        CancelInvoke("DoChangeScene");
+        Invoke("DoChangeScene", blinders.GetDuration());
+    }
+
+    void DoChangeScene()
+    {
+        // Debug.Log("Changing scene");
+        operation = SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+
+    //[UnityEditor.Callbacks.DidReloadScripts]
+    //private static void OnScriptsReloaded()
+    //{
+    //    if(UnityEditor.EditorApplication.isPlaying)
+    //    {
+    //        SceneHelper.ReloadScene();
+    //    }
+    //}
+}
